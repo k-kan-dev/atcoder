6cc1031b1e890d8bc304f708ef648a7a37fbc2c1
kazumasa kan <k.kan.splended@gmail.com>
2023-04-28 12:31:53 +0900

    add: 292-c ans‘

--- abc/292/c/ans.py
+++ abc/292/c/ans.py
@@ -1,23 +1,13 @@
 n = int(input())
 
-def get_num_of_divisors(x):
-    upper_divisors, lower_divisiors = set(), set()
-    i = 1
-    while i ** 2 <= x:
-         if x % i == 0:
-            lower_divisiors.add(i)
-            if (x // i) != i:
-                upper_divisors.add(x//i)
-         i += 1
-    return len(lower_divisiors | upper_divisors)
+nl = [1] * (n + 1)
+
+for i in range(2, n):
+    for j in range(i, n + 1, i):
+        nl[j] += 1
 
 ans = 0
-for ni in range(1, n//2 + 1):
-    ab = get_num_of_divisors(ni)
-    cd = get_num_of_divisors(n-ni)
-    if ni == n//2 and n % 2 == 0:
-        ans += ab * cd
-    else:
-        ans += ab * cd * 2
+for i in range(1, n):
+    ans += nl[i] * nl[n - i]
 
 print(ans)
